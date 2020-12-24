40c28940d25503ca9a01ed9a97e89cb6d693e768
Hu, Justin Jiaxing <jhu357@gatech.edu>
2020-12-23 20:47:51 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+var width = 1300;
+var height = 800;
+
+d3.csv("LeagueofLegends.csv", function(csv) {
+    var testData = d3
+                        .nest()
+                        .key(function (d) {
+                            return d.Year
+                        })
+                        .key(function (d) {
+                            if (d.blueTeamTag == "TSM") {
+                                if (d.bResult == 1) {
+                                    return "TSM_WIN";
+                                } else {
+                                    return "TSM_LOSS";
+                                }
+                            } else if ( d.redTeamTag == "TSM") {
+                                if (d.rResult == 1) {
+                                    return "TSM_WIN";
+                                } else {
+                                    return "TSM_LOSS";
+                                }
+                            } else {
+                                return "Other";
+                            }
+                        })
+                        .object(csv);
+    console.log(testData);
+    var tsm_2015 = testData["2015"];
+    var tsm_2016 = testData["2016"];
+    var tsm_2017 = testData["2017"];
+    console.log(tsm_2015);
+    console.log(tsm_2016);
+    console.log(tsm_2017);
+    var tsm_2015_total = tsm_2015["TSM_LOSS"].length + tsm_2015["TSM_WIN"].length;
+    var tsm_2015_wr = Math.round(tsm_2015["TSM_WIN"].length / tsm_2015_total * 100);
+    var tsm_2016_total = tsm_2016["TSM_LOSS"].length + tsm_2016["TSM_WIN"].length;
+    var tsm_2016_wr = Math.round(tsm_2016["TSM_WIN"].length / tsm_2016_total * 100);
+    var tsm_2017_total = tsm_2017["TSM_LOSS"].length + tsm_2017["TSM_WIN"].length;
+    var tsm_2017_wr = Math.round(tsm_2017["TSM_WIN"].length / tsm_2017_total * 100);
+    console.log(tsm_2015_wr);
+    console.log(tsm_2016_wr);
+    console.log(tsm_2017_wr);
+    var tsm_wr_map = [];
+    tsm_wr_map.push({x : "2015", y : tsm_2015_wr});
+    tsm_wr_map.push({x : "2016", y : tsm_2016_wr});
+    tsm_wr_map.push({x : "2017", y : tsm_2017_wr});
+    console.log(tsm_wr_map);
+    var chart;
+    generateGraph();
+
+    function generateGraph() {
+
+        var max = Math.max(tsm_2015_wr, tsm_2016_wr, tsm_2017_wr);
+
+        var xDomain = ["2015", "2016", "2017"];
+        var xScale = d3.scaleBand().domain(xDomain).range([0, 400, 800]).padding(0.4);
+        var xAxis = d3.axisBottom().scale(xScale);
+
+        var yScale = d3.scaleLinear().domain([0, max + 3]).range([400, 0]);
+        var yAxis = d3.axisLeft().scale(yScale);
+
+        chart = d3
+                        .select("#chart")
+                        .append("svg")
+                        .attr("width", width)
+                        .attr("height", height)
+                        .style("display", "block")
+                        .style("margin", "auto");
+        //x axis
+        chart
+                        .append("g")
+                        .attr("transform", "translate(440,700)")
+                        .call(xAxis)
+                        .append("text")
+                        .attr("class", "label")
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+        //y axis
+        chart
+                        .append("g")
+                        .attr("transform", "translate(440, 300)")
+                        .call(yAxis)
+                        .append("text")
+                        .attr("class", "label")
+                        .attr("transform", "rotate(-90)")
+                        .style("text-anchor", "end");
+
+        
+    }
+})
